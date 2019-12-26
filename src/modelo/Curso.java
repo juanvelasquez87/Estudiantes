0554097693aa1cbbf847c77e49bce92eeb4691c1
Juan Sebastian Velasquez Fernandez <jsvelasquez@personalsoft.com.co>
2019-12-26 14:18:15 -0500

    se agregaron constructores a la clase curso

--- src/modelo/Curso.java
+++ src/modelo/Curso.java
@@ -8,5 +8,38 @@ public class Curso {
     private int creditos;
 
 
+    public Curso(String codigo, String nombre, int credito){
+        this.codigoCurso = codigo;
+        this.nombreCurso = nombre;
+        this.creditos = credito;
+    }
+
+    public int getCreditos(){
+        return creditos;
+    }
+
+    public void setCreditos(int nuevosCreditos){
+        this.creditos = nuevosCreditos;
+    }
+
+    public double getNota(){
+        return nota;
+
+    }
+
+    public void setNota(double nota){
+        this.nota = nota;
+    }
+
+    public String getNombreCurso(){
+        return  nombreCurso;
+    }
+
+    public String getDescription() {
+        return "";
+    }
+    public boolean estaCalificacion(){
+        return false;
+    }
 
 }
